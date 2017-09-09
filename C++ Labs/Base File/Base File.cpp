c24c1ce1bfb94d7e8fe47dda3d84f45dea50e62f
WaleedRanaC <31806265+WaleedRanaC@users.noreply.github.com>
2017-09-09 14:40:41 -0500

    Add files via upload

---
+++ C++ Labs/Base File/Base File.cpp
@@ -0,0 +1,17 @@
+//
+// Waleed Rana
+// Base File (Project Name)
+// (Date)
+// (Whats in the project)
+//
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+
+int main()
+{
+	
+	return 0;
+}
